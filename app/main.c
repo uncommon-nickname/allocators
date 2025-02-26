baeed3a962cd4a9916d0b11b99745aef94738d6d
WiktorNowak <wnowak2f@gmail.com>
2025-02-26 18:47:17 +0100

    chore: initial build setup

---
+++ app/main.c
@@ -0,0 +1,7 @@
+#include "linear.h"
+
+int main(void)
+{
+    say_hello();
+    return 0;
+}
---
+++ include/linear.h
@@ -0,0 +1,8 @@
+#ifndef _LINEAR_ALLOC
+#define _LINEAR_ALLOC
+
+#include <stdio.h>
+
+static void say_hello() { printf("Hello, World!"); }
+
+#endif
