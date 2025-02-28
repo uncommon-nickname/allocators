1f44359fe836091d1fe5964035653096d7020855
WiktorNowak <wnowak2f@gmail.com>
2025-02-28 09:07:25 +0100

    fix: make arena allocator allocate bigger chunks

--- include/arena.h
+++ include/arena.h
@@ -1,13 +1,15 @@
 #ifndef _ARENA_H
 #define _ARENA_H
 
+#include <assert.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <sys/mman.h>
 
 #define UNMAP_SUCCESS 0
-#define MAX_REGION_SIZE 1024 * 8
-#define MIN(x, y) (((x) > (y)) ? (x) : (y))
+#define MAX_REGION_SIZE_CHUNKS 1024
+
+#define MAX(x, y) (((x) > (y)) ? (x) : (y))
 
 typedef struct Region Region;
 typedef struct Arena Arena;
@@ -16,6 +18,7 @@ struct Region
 {
     Region* next;
     size_t capacity;
+    size_t buff_ptr;
     uintptr_t buffer[];
 };
 
@@ -27,46 +30,69 @@ struct Arena
 
 Region* region_alloc(size_t capacity)
 {
-    size_t size    = sizeof(Region) + sizeof(uintptr_t) * capacity;
-    Region* region = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
+    size_t size_bytes = sizeof(Region) + sizeof(uintptr_t) * capacity;
+    Region* region = mmap(NULL, size_bytes, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
 
     if (region == MAP_FAILED)
         return NULL;
 
     region->next     = NULL;
     region->capacity = capacity;
+    region->buff_ptr = 0;
 
     return region;
 }
 
 int region_free(Region* r)
 {
-    size_t size = sizeof(Region) + sizeof(uintptr_t) * r->capacity;
-    return munmap(r, size);
+    size_t size_bytes = sizeof(Region) + sizeof(uintptr_t) * r->capacity;
+    return munmap(r, size_bytes);
 }
 
-void* arena_alloc(Arena* a, size_t size)
+void* arena_alloc(Arena* a, size_t size_bytes)
 {
-    if (size == 0)
-        return NULL;
+    assert(size_bytes != 0 && "Amount of bytes to allocate has to be greater than zero.");
 
-    size_t capacity = MIN(MAX_REGION_SIZE, size);
-    Region* region  = region_alloc(capacity);
-
-    if (region == NULL)
-        return NULL;
+    size_t size_chunks = (size_bytes - 1 + sizeof(uintptr_t)) / sizeof(uintptr_t);
 
     if (a->end == NULL)
     {
+        size_t capacity = MAX(MAX_REGION_SIZE_CHUNKS, size_chunks);
+        Region* region  = region_alloc(capacity);
+
+        if (region == NULL)
+            return NULL;
+
         a->end   = region;
         a->begin = region;
     }
-    else
+
+    Region* curr_region = a->begin;
+
+    while (curr_region != NULL)
     {
-        a->end->next = region;
-        a->end       = region;
+        if (curr_region->buff_ptr + size_chunks < curr_region->capacity)
+            break;
+
+        curr_region = curr_region->next;
     }
-    return &a->end->buffer[0];
+
+    if (curr_region == NULL)
+    {
+        size_t capacity = MAX(MAX_REGION_SIZE_CHUNKS, size_chunks);
+        curr_region     = region_alloc(capacity);
+
+        if (curr_region == NULL)
+            return NULL;
+
+        a->end->next = curr_region;
+        a->end       = curr_region;
+    }
+
+    void* result = &curr_region->buffer[curr_region->buff_ptr];
+    curr_region->buff_ptr += size_chunks;
+
+    return result;
 }
 
 int arena_free(Arena* a)
--- include/linear.h
+++ include/linear.h
@@ -1,26 +1,31 @@
 #ifndef _LINEAR_ALLOC
 #define _LINEAR_ALLOC
 
+#include <assert.h>
 #include <stddef.h>
 #include <stdint.h>
 
-#define LINEAR_MEMORY_SIZE 1024 * 10
+#define LINEAR_MEMORY_SIZE_CHUNKS 1024
 
 typedef struct Linear Linear;
 
 struct Linear
 {
-    size_t curr_idx;
-    uintptr_t data[LINEAR_MEMORY_SIZE];
+    size_t buff_ptr;
+    uintptr_t buffer[LINEAR_MEMORY_SIZE_CHUNKS];
 };
 
-void* linear_alloc(Linear* l, size_t size)
+void* linear_alloc(Linear* l, size_t size_bytes)
 {
-    if (l->curr_idx + size > LINEAR_MEMORY_SIZE || size == 0)
+    assert(size_bytes != 0 && "Amount of bytes to allocate has to be greater than zero.");
+
+    size_t size_chunks = (size_bytes - 1 + sizeof(uintptr_t)) / sizeof(uintptr_t);
+
+    if (l->buff_ptr + size_chunks > LINEAR_MEMORY_SIZE_CHUNKS)
         return NULL;
 
-    void* mem = &l->data[l->curr_idx];
-    l->curr_idx += size;
+    void* mem = &l->buffer[l->buff_ptr];
+    l->buff_ptr += size_chunks;
 
     return mem;
 }
